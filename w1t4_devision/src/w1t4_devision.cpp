51eb84d656132c68cdeb8b34f7a9fa459d1cfec8
Pavel Goryansky <pashikg54@gmail.com>
2021-03-29 16:11:18 +0300

    [w1t4] Implemented w1t4 task Devision.

---
+++ w1t4_devision/src/w1t4_devision.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : w1t4_devision.cpp
+// Author      : GPV
+// Version     : 1
+// Copyright   : none
+// Description : Devision of two unsigned number in range [ 0 .. 1 000 000 ]
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned int A, B, result;
+
+	cin >> A >> B;
+
+	// test for devision on ZERO
+	if ( B == 0 ){
+		cout << "Impossible";
+	} else {
+		 cout << (A / B);
+	}
+
+	return 0;
+}

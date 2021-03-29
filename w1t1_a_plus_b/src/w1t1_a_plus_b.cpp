18f71beea047244d5c3d409651f904a8c89c4b7d
Pavel Goryansky <pashikg54@gmail.com>
2021-03-29 10:27:59 +0300

    Formatted header Implemented w1t1 A+B test task.

--- w1t1_a_plus_b/src/w1t1_a_plus_b.cpp
+++ w1t1_a_plus_b/src/w1t1_a_plus_b.cpp
@@ -1,15 +1,20 @@
 //============================================================================
 // Name        : w1t1_a_plus_b.cpp
 // Author      : GPV
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Version     : 1
+// Copyright   : none
+// Description : Week 1, Task 1, A+B
 //============================================================================
 
 #include <iostream>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int d1, d2;
+	//cout << "Please enter two digits..." << endl;
+	// 1. digits no more than abs(100000)
+	cin >> d1 >> d2;
+	// 2. we not need to check 1-st condition
+	cout << (d1 + d2);
 	return 0;
 }

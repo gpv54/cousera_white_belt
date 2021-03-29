b2e2b18e0ea5f305b49ec6819589a560a580a474
Pavel Goryansky <pashikg54@gmail.com>
2021-03-29 13:41:04 +0300

    Implemented w1t3 task Square equation.

---
+++ w1t3_equation/src/w1t3_equation.cpp
@@ -0,0 +1,46 @@
+//============================================================================
+// Name        : w1t3_equation.cpp
+// Author      : GPV
+// Version     : 1
+// Copyright   : none
+// Description : Square equation
+//============================================================================
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	double D;
+	double A, B, C;
+	double x1, x2;
+
+	// Get a, b, c
+	cin >> A >> B >> C;
+
+
+	if ( A == 0 ){
+	// A=0, Bx + C = 0;
+		x1 = -C / B;
+		cout << x1 << endl;
+	} else {
+	// Ax2 + Bx + C = 0, Check D && find results
+		D = B*B - 4 * A * C;
+		if(D > 0){
+				// We are have 2 result
+				x1 = (-B + sqrt(D)) / ( 2 * A);
+				x2 = (-B - sqrt(D)) / ( 2 * A);
+				cout << x1 << " " << x2 << endl;
+			} else if (D == 0) {
+				// We are have 1 result
+				x1 = (-B + sqrt(D)) / ( 2 * A);
+				cout << x1 << endl;
+			}
+		// else {
+		//		cout "We don't have result."
+		// }
+	}
+
+	return 0;
+}

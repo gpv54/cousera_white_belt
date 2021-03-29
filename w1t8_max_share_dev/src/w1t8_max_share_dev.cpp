dac92695988c68797a410acb0705d36b11adcd1f
Pavel Goryansky <pashikg54@gmail.com>
2021-03-29 23:11:48 +0300

    [w1t8] Implemented w1t8 task.

---
+++ w1t8_max_share_dev/src/w1t8_max_share_dev.cpp
@@ -0,0 +1,42 @@
+//============================================================================
+// Name        : w1t8_max_share_dev.cpp
+// Author      : GPV
+// Version     : 1
+// Copyright   : none
+// Description : Max share div
+//============================================================================
+
+#include <iostream>
+//#include <ctime>
+
+using namespace std;
+
+
+int main() {
+	clock_t the_time;
+//	double elapsed_time;
+
+	long a, b, min;
+
+	the_time = clock();
+
+	cin >> a >> b;
+
+	 while ( a && b){
+		 if (a > b){
+			 //long swp = a;
+			 a = a % b;
+		 } else {
+			 b = b % a;
+		 }
+		 if( (b - a) > 0)
+			 b = b - a;
+	 }
+
+	 cout << (a + b) << endl;
+
+	 //elapsed_time = double(clock() - the_time);
+	 //std::cout << "Elapsed time " << elapsed_time << " ticks." << std::endl;
+
+	return 0;
+}
